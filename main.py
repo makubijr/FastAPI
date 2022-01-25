9f1fb6f1c2e51bd1b3375921e940b0e3f13bffd1
cipher <fynhacker@gmail.com>
2022-01-25 19:38:19 +0300

    getting one post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from random import randrange
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
 from typing import Optional
@@ -14,6 +15,13 @@ class Post(BaseModel):
 
 my_posts =[{"title":"this is the fist title","content":"this is the first content","id":1},{"title":"this is the second title","content":"this is the second content","id":2}]
 
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -25,5 +33,14 @@ def get_posts():
 
 @app.post("/posts")
 def create_posts(post:Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,1000000)
+    my_posts.append(post_dict)
     print(post)
-    return{"Data":post}
+    return{"Data":post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = find_post(id)
+    return{"post_detail": post}
